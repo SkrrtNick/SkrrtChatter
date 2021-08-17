b33fc4b9906d1d52c964e9f116600b779eff7ed5
SkrrtNick <79165336+SkrrtNick@users.noreply.github.com>
2021-08-17 14:18:19 +0100

    Update fxml url

--- src/scripts/SkrrtChatter.java
+++ src/scripts/SkrrtChatter.java
@@ -13,6 +13,7 @@ import org.tribot.script.interfaces.Arguments;
 import org.tribot.script.interfaces.Ending;
 import org.tribot.script.interfaces.Painting;
 import org.tribot.script.interfaces.Starting;
+import org.tribot.util.Util;
 import scripts.data.*;
 
 import scripts.api.data_tracker.DataTracker;
@@ -25,18 +26,28 @@ import scripts.api.util.functions.Traversing;
 import scripts.api.util.numbers.Randomisation;
 import scripts.api.util.numbers.Reactions;
 import scripts.api.util.numbers.SeedGenerator;
+import scripts.gui.GUI;
 import scripts.tasks.Chatter;
+import scripts.utilities.FileUtilities;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
+
+import static scripts.data.Vars.*;
 
 
 public class SkrrtChatter extends Script implements Starting, PaintInfo, Painting, Arguments, Ending {
 
 
     @ScriptManifest(name = "SkrrtChatter", authors = {"SkrrtNick"}, category = "Tools")
-
+    private boolean launchGUI = true;
+    private URL fxml;
+    private GUI gui;
     private final FluffeesPaint SkrrtPaint = new FluffeesPaint(this, FluffeesPaint.PaintLocations.BOTTOM_LEFT_PLAY_SCREEN, new Color[]{new Color(255, 251, 255)}, "Trebuchet MS", new Color[]{new Color(0, 0, 0, 124)},
             new Color[]{new Color(179, 0, 0)}, 1, false, 5, 3, 0);
     DataTracker tracker = new DataTracker("https://api.skrrtscripts.com", "secret", "chatter");
@@ -44,6 +55,20 @@ public class SkrrtChatter extends Script implements Starting, PaintInfo, Paintin
 
     @Override
     public void run() {
+
+        if (launchGUI) {
+            try {
+                fxml = new URL("https://raw.githubusercontent.com/SkrrtNick/SkrrtChatter/master/src/scripts/gui/gui.fxml");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            gui = new GUI(fxml);
+            gui.show();
+            while (gui.isOpen()) {
+                sleep(500);
+            }
+        }
+
         Logging.message("SkrrtChatter","Welcome to SkrrtChatter","Interacting with " + Vars.npcName + " via " + Vars.action,"Navigating to: " + "x: " + Vars.x + " y: " + Vars.y + " z: " + Vars.z);
         if(!Vars.dialogue.isEmpty()){
             Logging.message("SkrrtChatter","Dialogue: " + Vars.dialogue.toString(),"Dialogue size: " + (Vars.dialogue.size()));
@@ -75,7 +100,7 @@ public class SkrrtChatter extends Script implements Starting, PaintInfo, Paintin
 
     @Override
     public String[] getPaintInfo() {
-        return new String[]{"SkrrtChatter V0.91", "Time ran: " + SkrrtPaint.getRuntimeString(), "Status: " + Core.getStatus()};
+        return new String[]{"SkrrtChatter V1.0", "Time ran: " + SkrrtPaint.getRuntimeString(), "Status: " + Core.getStatus()};
     }
 
 
@@ -86,27 +111,31 @@ public class SkrrtChatter extends Script implements Starting, PaintInfo, Paintin
 
     @Override
     public void passArguments(HashMap<String, String> hashMap) {
+        Core.setProfileDirectory("/Skrrt/Chatter/Profiles");
+        FileUtilities.createProfileDirectory(Core.getProfileDirectory());
         String scriptSelect = hashMap.get("custom_input");
         String clientStarter = hashMap.get("autostart");
         String input = clientStarter != null ? clientStarter : scriptSelect;
-        String[] settings = input.split("(?<!%),");
+        String[] settings = input.split(",");
         if (settings.length > 0) {
             for (String s : settings) {
-                if (s.contains("settings:" )&& s.contains(";")) {
-                    Vars.npcName = s.split(":")[1] != null ? s.split(":")[1] : null;
-                    Vars.action = s.split(":")[2] != null ? s.split(":")[2] : null;
-                    Vars.x = s.split(":")[3] != null ? Integer.parseInt(s.split(":")[3]) : null;
-                    Vars.y = s.split(":")[4] != null ? Integer.parseInt(s.split(":")[4]) : null;
-                    Vars.z = s.split(":")[5] != null ? Integer.parseInt(s.split(":")[5]) : null;
-                    Vars.dialogue.addAll(Arrays.asList(s.split(":")[6].replaceAll("%","").split(";")));
-                } else if (s.contains("settings:")){
-                    Vars.npcName = s.split(":")[1] != null ? s.split(":")[1] : null;
-                    Vars.action = s.split(":")[2] != null ? s.split(":")[2] : null;
-                    Vars.x = s.split(":")[3] != null ? Integer.parseInt(s.split(":")[3]) : null;
-                    Vars.y = s.split(":")[4] != null ? Integer.parseInt(s.split(":")[4]) : null;
-                    Vars.z = s.split(":")[5] != null ? Integer.parseInt(s.split(":")[5]) : null;
+                if (s.contains("settings:")) {
+                    profileName = s.split(":")[1] != null ? s.split(":")[1] : null;
                 }
-            } Vars.isRunning = true;
+            }
+        }
+        if (profileName != null) {
+            if (!profileName.endsWith(".json")) {
+                profileName += ".json";
+            }
+            try {
+                runtimeSettings = FileUtilities.gson.fromJson(new String(FileUtilities.loadFile(new File(Util.getWorkingDirectory().getAbsolutePath() + Core.getProfileDirectory() + "/" + profileName))), Profile.class);
+                launchGUI = false;
+                runningPrep = true;
+            } catch (IOException e) {
+                Logging.debug(e.getMessage());
+                Logging.debug("Unable to locate profile");
+            }
         }
     }
 
